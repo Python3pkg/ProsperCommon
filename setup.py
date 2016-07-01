28a25314b6e2fbae1612a73b56e9d177a3a17518
John Purcell <John Purcell>
2016-07-01 08:26:19 -0700

    adding setup.py for common utilities
    TODO: test wheel for installing into other projects

---
+++ setup.py
@@ -0,0 +1,24 @@
+'''wheel setup for Prosper common utilities'''
+
+from os import path
+from setuptools import setup, find_packages
+
+
+HERE = path.abspath(path.dirname(__file__))
+
+setup(
+    name='ProsperCommon',
+    version='0.0.1',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.5'
+    ],
+    keywords='prosper eveonline api CREST',
+    packages=find_packages(),
+    package_data={
+        #TODO
+    },
+    install_requires=[
+        #TODO
+    ]
+)
