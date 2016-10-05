42639b0eeac8e8c14ae766bc4af0e6a455802902
Lockefox <locke.renard@gmail.com>
2016-10-04 21:03:51 -0700

    removed LoggerDebugger from common
    useless functions that were worse than the default logger

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def hack_find_packages(include_str):
 
 setup(
     name='ProsperCommon',
-    version='0.0.5',
+    version='0.0.6',
     license='MIT',
     classifiers=[
         'Programming Language :: Python :: 3.5'
