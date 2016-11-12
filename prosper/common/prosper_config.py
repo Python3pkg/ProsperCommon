799495f2134d5466c88ae0c1097e384fd122b3ac
Lockefox <locke.renard@gmail.com>
2016-11-11 17:58:44 -0800

    adding skelleton for ProsperConfig parser/checker

--- prosper/common/prosper_config.py
+++ prosper/common/prosper_config.py
@@ -1,16 +1,63 @@
-'''prosper_config.py standardized configparser for prosper'''
+"""prosper_config.py
+
+Unified config parsing and option picking against config objects
+
+"""
 
 from os import path
 import configparser
 from configparser import ExtendedInterpolation
+import warnings
+import logging
 
 HERE = path.abspath(path.dirname(__file__))
+
+DEFAULT_LOGGER = logging.getLogger('NULL')
+DEFAULT_LOGGER.addHandler(logging.NullHandler())
+
+class ProsperConfig(object):
+    """configuration handler for all prosper projects
+
+    Helps maintain global, local, and args values to pick according to priority
+
+    1. args given at runtile
+    2. <config_file>_local.cfg -- untracked config with #SECRETS
+    3. <config_file>.cfg -- tracked 'master' config without #SECRETS
+
+    Attributes:
+        global_config (:obj:`configparser.ConfigParser`)
+        local_config (:obj:`configparser.ConfigParser`)
+        global_cache (:obj:`dict`) volatile stash of read configs
+        local_cache (:obj:`dict`) volatile stash of read configs
+
+    """
+    _debug_mode = False
+    def __init__(
+            self,
+            config_filename,
+            logger=DEFAULT_LOGGER,
+            debug_mode=_debug_mode
+    ):
+        """get the config filename for initializing data structures
+
+        Args:
+            config_filename (str): path to config
+            logger (:obj:`logging.Logger`, optional): capture messages to logger
+            debug_mode (bool, optional): enable debug modes for config helper
+
+        """
+        pass
+
 
 def get_config(
         config_filepath,
         local_override=False
 ):
-    '''returns config object for parsing global values'''
+    """DEPRECATED: classic v1 config parser.  Obsolete by v0.3.0"""
+    warnings.warn(
+        __name__ + 'replaced with ProsperConfig',
+        DeprecationWarning
+    )
     config = configparser.ConfigParser(
         interpolation=ExtendedInterpolation(),
         allow_no_value=True,
