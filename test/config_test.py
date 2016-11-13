4c01de6f28c3be57f39542e8dc76d7a45d280cbe
Lockefox <locke.renard@gmail.com>
2016-11-13 12:03:10 -0800

    adding bad_config test and fixing config_test filepathing

--- test/config_test.py
+++ test/config_test.py
@@ -7,23 +7,44 @@ import prosper.common.prosper_config as prosper_config
 import prosper.common.prosper_utilities as prosper_utilities
 
 HERE = path.abspath(path.dirname(__file__))
-LOCAL_CONFIG = path.join(HERE, 'common_config.cfg') #use /test config
-if not path.isfile(LOCAL_CONFIG):   #else use /prosper/common config
-    DEFAULT_PATH = path.join(path.dirname(HERE), 'prosper', 'common')
-    LOCAL_CONFIG = path.join(DEFAULT_PATH, 'common_config.cfg')
+ROOT = path.dirname(HERE)
+LOCAL_CONFIG_PATH = path.join(
+    ROOT,
+    'prosper',
+    'common',
+    'common_config.cfg'
+)
+
 
 TEST_LOCAL_CONFIG_PATH = path.join(HERE, 'test_config_local.cfg')
-TEST_LOCAL_CONFIG = prosper_utilities.read_config(TEST_LOCAL_CONFIG_PATH)
+TEST_LOCAL_CONFIG = prosper_config.read_config(TEST_LOCAL_CONFIG_PATH)
 
 TEST_GLOBAL_CONFIG_PATH = path.join(HERE, 'test_config_global.cfg')
-TEST_GLOBAL_CONFIG = prosper_utilities.read_config(TEST_GLOBAL_CONFIG_PATH)
+TEST_GLOBAL_CONFIG = prosper_config.read_config(TEST_GLOBAL_CONFIG_PATH)
+
+TEST_BAD_CONFIG_PATH = path.join(HERE, 'bad_config.cfg')
+TEST_BAD_PATH = path.join(HERE, 'no_file_here.cfg')
+def test_bad_config():
+    """failing test: bad parsing"""
+    test_config = prosper_config.read_config(TEST_BAD_CONFIG_PATH)
+
+    ## Test keys with bad delimters
+    with pytest.raises(KeyError):
+        test_config['TEST']['key3']
+        test_config['FAILS']['shared_key']
+
+    ## Assert good keys are working as expected
+    good_val = test_config['TEST']['key1']
+    assert good_val == 'vals'
+
+    ## Test behavior with bad filepath
+    with pytest.raises(FileNotFoundError):
+        bad_config = prosper_config.read_config(TEST_BAD_PATH)
 
 def test_config_file():
-    '''make sure local/tracked config files mesh'''
-    unique_values = prosper_utilities.compare_config_files(LOCAL_CONFIG)
+    """Test makes sure tracked/local configs have all matching keys"""
+    unique_values = prosper_utilities.compare_config_files(LOCAL_CONFIG_PATH)
 
-    #if local_debug:
-    #    return unique_values
     message = ''
     if unique_values:
         message = json.dumps(
@@ -33,7 +54,7 @@ def test_config_file():
         )
 
     if message:
-        assert False, message
+        assert False, message #FIXME: this seems wrong
 
 if __name__ == '__main__':
     print(test_config_file())
