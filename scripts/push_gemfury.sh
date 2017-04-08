ed900edaa1ab41eff700faf09647ebb23072e286
Lockefox <locke.renard@gmail.com>
2017-04-07 17:26:58 -0700

    debugging CD step some more

--- scripts/push_gemfury.sh
+++ scripts/push_gemfury.sh
@@ -1,4 +1,12 @@
 echo 'BUILDING FOR RELEASE'
 python setup.py sdist bdist_wheel clean --all
-echo $PWD
-curl -F package=@dist/ProsperCommon-*tar.gz $GEMFURY_URL
+echo 'UPLOADING TO GEMFURY'
+for file in $PWD/dist/*
+do
+	if [[ $file =~ tar.gz ]]
+		then 
+			echo $file
+			curl -F package=@$file $GEMFURY_URL
+		fi
+done
+
